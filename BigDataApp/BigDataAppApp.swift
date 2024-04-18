49a85569d7e82560c99101a75c972b734cf0bea8
Alexander Litvinov <allidemocracy@gmail.com>
2024-04-18 14:46:07 +0300

    Initial Commit

---
+++ BigDataApp/BigDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigDataAppApp.swift
+//  BigDataApp
+//
+//  Created by Main on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BigDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
